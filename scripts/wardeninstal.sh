370d4e710daf7a6c13d5075d6fc0d8576c99aa52
NomadJobo <jon@bownds.com>
2023-10-13 13:03:26 -0500

    adding warden

---
+++ scripts/wardeninstal.sh
@@ -0,0 +1,7 @@
+rm -rf /opt/warden/
+sudo mkdir /opt/warden
+sudo chown $(whoami) /opt/warden
+git clone -b develop https://github.com/davidalger/warden.git /opt/warden
+echo 'export PATH="/opt/warden/bin:$PATH"' >> ~/.bashrc
+PATH="/opt/warden/bin:$PATH"
+warden svc up
