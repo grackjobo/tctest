28113292cc25932a1ca04bae0fe8e5aca049d87e
NomadJobo <jon@bownds.com>
2023-10-14 22:35:52 -0500

    adding mutagen dependency

--- scripts/wardeninstall.sh
+++ scripts/wardeninstall.sh
@@ -1,7 +1,8 @@
-rm -rf /opt/warden/
+brew install havoc-io/mutagen/mutagen
+sudo rm -rf /opt/warden/
 sudo mkdir /opt/warden
 sudo chown $(whoami) /opt/warden
-git clone -b develop https://github.com/davidalger/warden.git /opt/warden
+sudo git clone -b develop https://github.com/davidalger/warden.git /opt/warden
 echo 'export PATH="/opt/warden/bin:$PATH"' >> ~/.bashrc
 PATH="/opt/warden/bin:$PATH"
 sudo warden svc up
