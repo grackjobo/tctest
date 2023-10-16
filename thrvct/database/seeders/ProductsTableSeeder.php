3216879fcd241ff7e7d43758ec4a31329a0d1144
Jonathan Bownds <jon@bownds.com>
2023-10-16 12:01:04 -0500

    Added laravel details

---
+++ thrvct/database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+public function run()
+{
+    \DB::table('products')->insert([
+        ['code' => 'R01', 'name' => 'Red Widget', 'price' => 32.95],
+        ['code' => 'G01', 'name' => 'Green Widget', 'price' => 24.95],
+        ['code' => 'B01', 'name' => 'Blue Widget', 'price' => 7.95],
+    ]);
+}
+
+}
