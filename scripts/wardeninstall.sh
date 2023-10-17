09e5195a3899ac231c5b55f60c7b7d9b3827fb57
Jonathan Bownds <jbownds@Jonathans-MBP.localdomain>
2023-10-17 18:32:02 -0500

    Updatedwardeninstall

--- scripts/wardeninstall.sh
+++ scripts/wardeninstall.sh
@@ -5,4 +5,5 @@ sudo chown $(whoami) /opt/warden
 sudo git clone -b develop https://github.com/davidalger/warden.git /opt/warden
 echo 'export PATH="/opt/warden/bin:$PATH"' >> ~/.bashrc
 PATH="/opt/warden/bin:$PATH"
+brew install wardenenv/warden/warden
 sudo warden svc up
